792947d97549d811e596f96730636b2ac330af4b
Eduardo <eduardosaldanhacs@gmail.com>
2025-03-20 10:45:54 -0300

    Ninth commit

--- index.php
+++ index.php
@@ -34,9 +34,8 @@ if (verificaLogin()) {
     include('modulos/login.php');
 }
 
-?>
-
-</body>
-<?php
 include('modulos/rodape.php');
 ?>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+<script src="<?= SITE ?>includes/scripts.js"></script>
--- modulos/rodape.php
+++ modulos/rodape.php
@@ -6,9 +6,3 @@
     </body>
 </html>
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-<script src="<?= SITE ?>includes/scripts.js"></script>
-
-
-
